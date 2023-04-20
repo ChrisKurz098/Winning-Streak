45a1157dad0bf5e449b0a78d226cb68e6e83bb4c
Chris Kurz <ckurz098@gmail.com>
2023-04-19 20:59:15 -0700

    added loading message

--- src/routes/Home/Home.components.jsx
+++ src/routes/Home/Home.components.jsx
@@ -1,13 +1,16 @@
+import { useContext } from "react";
 import CurrentGoals from "../../components/CurrentGoals/CurrentGoals.component.jsx";
 import GolaList from "../../components/GoalList/GoalList.component.jsx";
 import UserStats from "../../components/userStats/UserStats.component.jsx";
 
 
 import "./home.styles.scss"
+import { UserContext } from "../../contexts/user.context.jsx";
 
 const Home = () => {
+  const {currentUser} = useContext(UserContext)
 
-
+  if (!currentUser) return (<div className="loading-message"> <p> Gathering user data...</p></div>)
 
 
   return (
