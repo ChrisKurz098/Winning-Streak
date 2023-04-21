198a67424306465b5796bbd159e7c7d58d535bff
Chris Kurz <ckurz098@gmail.com>
2023-04-20 20:04:14 -0700

    Adjusted home page to display: block. Focusing on phone layout. Added New Goal button that links to /new

--- src/routes/Home/Home.components.jsx
+++ src/routes/Home/Home.components.jsx
@@ -6,23 +6,27 @@ import UserStats from "../../components/userStats/UserStats.component.jsx";
 
 import "./home.styles.scss"
 import { UserContext } from "../../contexts/user.context.jsx";
+import Button from "../../components/Button/Button.component.jsx";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const {currentUser} = useContext(UserContext)
+  const { currentUser } = useContext(UserContext)
+  const navigate = useNavigate();
 
   if (!currentUser) return (<div className="loading-message"> <p> Gathering user data...</p></div>)
 
 
   return (
     <div className="home-container">
-      <span className="goals-container">
-        <CurrentGoals />
-        <GolaList />
-      </span>
-
       <span className="stats-calander-container">
         <UserStats />
       </span>
+      <span className="goals-container">
+        <GolaList />
+        <Button buttonType='button' type='button' onClick={() => { navigate('/new') }}> Add New Goal</Button>
+      </span>
+
+
     </div>
   );
 };
