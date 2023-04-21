905085340d6f0b9cd57be40a8fdaab7737fc6b71
Chris Kurz <ckurz098@gmail.com>
2023-04-20 21:57:14 -0700

    ready to merge with dev

--- src/routes/CreateGoal/CreateGoal.component.jsx
+++ src/routes/CreateGoal/CreateGoal.component.jsx
@@ -5,10 +5,15 @@ import { goalTypes } from '../../utils/userData/userDataFunctions';
 import { UserContext } from '../../contexts/user.context'
 import FormInput from '../../components/FormInput/FormInput.component';
 import Button from '../../components/Button/Button.component';
+import { updateRemoteUserData } from '../../utils/firebase/firebase.utils';
+import { useNavigate } from 'react-router-dom';
 
 
 const CreateGoal = () => {
-    const { currentUser, setCurrentUser } = useContext(UserContext)
+    const { currentUser, setCurrentUser, userAuthId } = useContext(UserContext)
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate();
 
 
     const [formInput, setFormInput] = useState({
@@ -49,54 +54,58 @@ const CreateGoal = () => {
     };
 
 
-    const submitGoal = (event) => {
+    const submitGoal = async (event) => {
         event.preventDefault();
+        setLoading(true);
         let updatedUser = { ...currentUser };
-console.log(updatedUser);
+        console.log(updatedUser);
         updatedUser.userData.goals.push(formInput);
         console.log(updatedUser)
         setCurrentUser(updatedUser);
+        await updateRemoteUserData(userAuthId, currentUser.userData);
+        navigate('/');
+
     }
 
     return (
         <>
-   
-        <div className='create-goal-container'>
-            <form onSubmit={submitGoal}>
-            <h2 className='create-goal-header'>Create a new goal:</h2>
-                <label htmlFor='type-select'>Goal Type</label>
-                <select id='type-select' name='typeSelect' onChange={handleDropDownChange}>
-                    {
-                        goalTypes.map((type, i) => {
-                            return (
-                                <option key={`${type}-${i}`} value={type}>{`${type}`}</option>
-                            )
-                        })
-                    }
-                </select>
-                <FormInput label='Title' required value={title} name='title' onChange={handleChange} />
-                <FormInput label='Description' required value={description} name='description' onChange={handleChange} />
-                <FormInput label='Weekly Interval' type='number' required value={weeklyInterval} name='weeklyInterval' onChange={handleChange} />
-                <div className='goal-days-container'>
-                    {
-                        days.map((day, i) => {
-                            return (
-                                <div key={`${day}${i}`} className='day-container nowrap'>
-                                <input id={`${i}`} type='checkbox' checked={goalDays[i]} onChange={handleGoalDayChange} />
-                                <label htmlFor={`${i}`}>{`${day}`}</label>
-                                </div>
-                            )
-                        })
-                    }
 
-  
-                </div>
+            <div className='create-goal-container'>
+                <form onSubmit={submitGoal}>
+                    <h2 className='create-goal-header'>Create a new goal:</h2>
+                    <label htmlFor='type-select'>Goal Type</label>
+                    <select id='type-select' name='typeSelect' onChange={handleDropDownChange}>
+                        {
+                            goalTypes.map((type, i) => {
+                                return (
+                                    <option key={`${type}-${i}`} value={type}>{`${type}`}</option>
+                                )
+                            })
+                        }
+                    </select>
+                    <FormInput label='Title' required value={title} name='title' onChange={handleChange} />
+                    <FormInput label='Description' required value={description} name='description' onChange={handleChange} />
+                    <FormInput label='Weekly Interval' type='number' required value={weeklyInterval} name='weeklyInterval' onChange={handleChange} />
+                    <div className='goal-days-container'>
+                        {
+                            days.map((day, i) => {
+                                return (
+                                    <div key={`${day}${i}`} className='day-container nowrap'>
+                                        <input id={`${i}`} type='checkbox' checked={goalDays[i]} onChange={handleGoalDayChange} />
+                                        <label htmlFor={`${i}`}>{`${day}`}</label>
+                                    </div>
+                                )
+                            })
+                        }
 
-                <div className="buttons-container">
-                    <Button type='submit'>Add New Goal</Button>
-                </div>
-            </form>
-        </div>
+
+                    </div>
+
+                    <div className="buttons-container">
+                        {(loading) ? (<Button type='button'>Updating...</Button>) : (<Button type='submit'>Add New Goal</Button>)}
+                    </div>
+                </form>
+            </div>
         </>
     )
 };
