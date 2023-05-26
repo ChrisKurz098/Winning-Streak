66cf3bfd392c9f1efbb1c821e2df11feb6856eeb
Chris Kurz <ckurz098@gmail.com>
2023-05-25 17:05:07 -0700

    set up GH-pages for deployment

--- src/components/GoalList/MinGoalView/MinGoalView.componet.jsx
+++ src/components/GoalList/MinGoalView/MinGoalView.componet.jsx
@@ -1,5 +1,5 @@
 
-const MinGoalView = ({goal}) => {
+const MinGoalView = ({ goal }) => {
 
     const {
         typeSelect,
@@ -9,24 +9,22 @@ const MinGoalView = ({goal}) => {
     } = goal;
 
 
-return (
-    <div className="min-view-container">
-        <span>
-        <img alt='goal icon' src={`icons/${typeSelect}.png`} />
-    <img alt="check mark" className="check-mark" src={`icons/checkmark.png`} style={{opacity: (intervalComplete) ? '100%' : '0%' , animation: (intervalComplete) ? '' : 'none' }}/>
-        </span>
+    return (
+        <div className="min-view-container">
 
-    <h3
-        className="goal-list-item"
-    >{title}</h3>
+            <span className="goal-type-check">
+                <img alt='goal icon' src={`icons/${typeSelect}.png`} />
+                <img alt="check mark" className="check-mark" src={`icons/checkmark.png`} style={{ opacity: (intervalComplete) ? '100%' : '0%', animation: (intervalComplete) ? '' : 'none' }} />
+            </span>
+            <h3 className="goal-list-title">{title}</h3>
+       
+            <span className="streak-display-container">
+                <h3>Streak:</h3>
+                <p>{(currentStreak) ? currentStreak : 0}</p>
+            </span>
+        </div>
 
-    <span className="streak-display-container">
-        <h3>Streak:</h3>
-        <p>{(currentStreak) ? currentStreak : 0}</p>
-    </span>
-</div>
-
-)
+    )
 };
 
 export default MinGoalView;
