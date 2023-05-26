139a21d8eeeab5b4142be7eaf31e33297259074d
Chris Kurz <ckurz098@gmail.com>
2023-05-26 10:39:56 -0700

    temp api key

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -27,8 +27,8 @@ import { userDataTemplate } from "../userData/userDataFunctions";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: "https://chriskurz098.github.io/Winning-Streak",
+    apiKey: "AIzaSyATXqKeF4xvPV19NOJJpoMebdwK34zKVHE",
+    authDomain: "winning-streak-64797.firebaseapp.com",
     projectId: "winning-streak-64797",
     storageBucket: "winning-streak-64797.appspot.com",
     messagingSenderId: "70058450446",
